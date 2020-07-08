f732d0ff532933b0b484e97a9abbbc4002162e55
Amir-anwar <super4amir@gmail.com>
2020-07-08 13:12:36 +0200

    serial generator

---
+++ practice-9-generate-random-serial-number/app.js
@@ -0,0 +1,23 @@
+/* eslint-disable require-jsdoc */
+const btn = document.querySelector('#btn');
+const show = document.querySelector('#show');
+
+
+function generateSerial() {
+  const
+    chars = '1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM';
+  const serialLength = 20;
+  let serial = '';
+
+  for (let i = 0; i < serialLength; i++) {
+    const randomNumber = Math.floor(Math.random() * serialLength);
+
+    const randomChar = chars.slice(randomNumber, randomNumber + 1);
+
+    serial += randomChar;
+  }
+
+  show.textContent = serial;
+};
+
+btn.addEventListener('click', generateSerial);
